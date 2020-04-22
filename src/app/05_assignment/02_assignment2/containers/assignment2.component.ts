582fe16e7b17ef67fbc2b62bcff5d24618f8d5bf
Pranav Harisinghani <pranav.harisinghani@nagarro.com>
2020-04-22 15:37:44 +0530

    Assignment for Angular

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/03_pipes/models/todo.interface';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  @Input() todos:Todo;
+
+  selectedRow:number;
+
+  constructor()
+  {console.log("Processing..");}
+
+  setRow(i){
+    this.selectedRow=i;
+  }
 
 }
